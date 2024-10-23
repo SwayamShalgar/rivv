d3d96e21c65b52e69664482659b81cc0a8994778
shoaib inamdar <shoaibinamdar14@gmail.com>
2024-10-24 00:15:27 +0530

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='w-full h-screen bg-zinc-900 flex justify-center items-center'>
+        <h1 className='text-white font-semibold text-4xl'>Coming soon...</h1>
+    </div>
+  )
+}
+
+export default Hero
